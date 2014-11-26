4b05f2c574c6dd90ea6466099170dc09ffba8126
warna720 <warhell1@hotmail.com>
2014-11-26 23:48:42 +0100

    Update Component.cc

--- lab6/Component.cc
+++ lab6/Component.cc
@@ -5,8 +5,6 @@
 Component::Component(const std::string & name, Connection & p, Connection & n) :
 					 _name(name), _a(&p), _b(&n) {}
 
-
-// Shut the fuck up compiler
 Component::~Component() {}
 
 
